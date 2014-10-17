9b69fad09bb51bed51e88108f6fce2a7953c4c8e
WangHaoRan <whrlmc@139.com>
2014-10-18 01:53:41 +0800

    23232

---
+++ application/controllers/welcome.php
@@ -0,0 +1,23 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+
+    public function shows(){
+        $this->load->helper('url');
+        $token_url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx4b9a0bd3865f2332&redirect_uri=' . urlencode('http://nokia.cnhtk.cn/redirect.php') . '&response_type=code&scope=snsapi_base&state=index#wechat_redirect';
+        redirect($token_url);
+    }
+
+    public function checks(){
+        var_dump($_GET);
+    }
+
+	public function index()
+	{
+		$this->load->view('welcome');
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
