6649dceff220e6ab2dd499e4da4d7c7e47af4cb3
WangHaoRan <whrlmc@139.com>
2014-10-18 02:33:16 +0800

    2323

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -4,15 +4,26 @@ class Welcome extends CI_Controller {
 
 
     public function shows(){
-        $this->load->helper('url');
-        $token_url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx4b9a0bd3865f2332&redirect_uri=' . urlencode('http://nokia.cnhtk.cn/xiaona/index.php/welcome/checks') . '&response_type=code&scope=snsapi_base&state=index#wechat_redirect';
-        redirect($token_url);
+
     }
 
     public function checks(){
         if(!$_GET['code']){
             die('Authorization failure!');
         }
+
+
+        //记录信息
+    }
+
+	public function index()
+	{
+        if(empty($_GET['code'])){
+            $this->load->helper('url');
+            $token_url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx4b9a0bd3865f2332&redirect_uri=' . urlencode('http://nokia.cnhtk.cn/xiaona') . '&response_type=code&scope=snsapi_base&state=index#wechat_redirect';
+            redirect($token_url);
+        }
+
         $token_url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx4b9a0bd3865f2332&secret=8359e84e67ca7138aa1342d23f32eb00&code=' . $_GET['code'] . '&grant_type=authorization_code';
         $result_json = file_get_contents($token_url);
         $result_arr = json_decode($result_json, true);
@@ -30,13 +41,6 @@ class Welcome extends CI_Controller {
         }
 
         $this->load->view('welcome', $data);
-
-        //记录信息
-    }
-
-	public function index()
-	{
-		$this->load->view('welcome');
 	}
 }
 
